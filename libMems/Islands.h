8af712943ccdb92b8fe8e593804e969eb023834b
Aaron Darling <aaron.darling@uts.edu.au>
2007-04-26 05:32:05 +0000

    Fixed left and right side assumed homology for random walk scoring Added an experimental MSC detection algorithm

--- libMems/Islands.h
+++ libMems/Islands.h
@@ -409,13 +409,200 @@ void findRightEndpoint( size_t seqI, size_t seqJ, score_t significance_threshold
 	}
 }
 
+
+inline
+void findHssExcursions( std::vector< score_t > scores, score_t significance_threshold, hss_list_t& hss_list, uint seqI, uint seqJ, boolean left_hss, boolean right_hss )
+{
+	score_t score_sum = left_hss ? significance_threshold : 0;	// start in an hss if non-homologous
+	int64 ladder_point = 0;
+	bool fwd_hss = left_hss;
+
+	// scan left to right over the columns to identify HSS
+	for( size_t colI = 0; colI <= scores.size(); ++colI )
+	{
+		if( colI < scores.size() && scores[colI] == INVALID_SCORE )
+			continue;
+
+		if( colI == scores.size() || (score_sum >= 0 && score_sum + scores[colI] < 0)  )
+		{
+			// end of an excursion
+			if( colI == scores.size() && right_hss )
+				fwd_hss = true;
+
+			score_sum = 0;
+			if( fwd_hss )
+			{
+				// call the entire segment between the current column and the ladder point
+				// an excursion
+				HssCols ic;
+				ic.seqI = seqI;
+				ic.seqJ = seqJ;
+				ic.left_col = ladder_point;
+				if( colI == scores.size() )
+					ic.right_col = colI - 1;
+				else
+					ic.right_col = colI;
+				hss_list.push_back( ic );
+			}
+			fwd_hss = false;
+		}else if( score_sum == 0 && scores[colI] > 0 )
+		{
+			// start a new excursion
+			score_sum += scores[colI];
+			ladder_point = colI;
+		}else
+			score_sum += scores[colI];
+
+		if( score_sum > significance_threshold )
+			fwd_hss = true;
+	}
+}
+
+
+inline
+void findMscFromExcursions( std::vector< score_t > scores, score_t significance_threshold, hss_list_t& hss_list, hss_list_t& msc_list, uint seqI, uint seqJ, boolean left_hss, boolean right_hss )
+{
+	score_t left_end_score = left_hss ? significance_threshold : 0;
+	score_t right_end_score = right_hss ? significance_threshold : 0;
+	score_t score_sum = left_end_score;	// start in an hss if non-homologous
+	int64 ladder_point = 0;
+	bool fwd_hss = true;
+	if( left_hss )
+		scores.front() = significance_threshold;
+	if( right_hss )
+		scores.back() = significance_threshold;
+
+	// for each excursion in hss_list
+	for( size_t exI = 0; exI < hss_list.size(); exI++ )
+	{
+		// create a vector of score sums
+		size_t col_base = hss_list[exI].left_col;
+		size_t col_count = hss_list[exI].right_col - hss_list[exI].left_col + 1;
+		// find cols with positive scores
+		size_t positive_count = 0;
+		for( size_t colI = hss_list[exI].left_col; colI < hss_list[exI].right_col + 1; colI++ )
+		{
+			if( scores[colI] <= 0 || scores[colI] == INVALID_SCORE  )
+				continue;
+			positive_count++;
+		}
+		std::vector< size_t > pos_map(positive_count);
+		positive_count = 0;
+		for( size_t colI = hss_list[exI].left_col; colI < hss_list[exI].right_col + 1; colI++ )
+		{
+			if( scores[colI] <= 0 || scores[colI] == INVALID_SCORE )
+				continue;
+			pos_map[positive_count] = colI;
+			positive_count++;
+		}
+
+		std::vector< score_t > score_sums(positive_count, 0);
+		size_t sum_base = 0;
+		std::vector<HssCols> cur_msc_list;
+		size_t invalid_count = 0;
+		for( size_t colI = hss_list[exI].left_col; colI < hss_list[exI].right_col + 1; colI++ )
+		{
+			// skip this column if it has an invalid score
+			if( scores[colI] == INVALID_SCORE )
+				continue;
+			// otherwise add the current column score to all relevant score sums
+			int64 msc_col = -1;
+			size_t sumI = sum_base;
+			for( ; sumI < score_sums.size(); sumI++ )
+			{
+				// break the loop if the next positive column is past colI
+				if( pos_map[sumI] > colI )
+					break;
+				// don't worry about this one if it's invalid
+				if( score_sums[sumI] < 0 )
+					continue;
+				score_sums[sumI] += scores[colI];
+				// if the local score bottoms out to 0 then this one has failed
+				if( score_sums[sumI] < 0 )
+					invalid_count++;
+				// take the right-most starting column which yields an msc
+				if( score_sums[sumI] >= significance_threshold )
+					msc_col = sumI;
+			}
+			// did we find a minimum significant cluster?
+			if( msc_col != -1 )
+			{
+				HssCols ic;
+				ic.seqI = seqI;
+				ic.seqJ = seqJ;
+				ic.left_col = pos_map[msc_col];
+				ic.right_col = colI;
+				cur_msc_list.push_back( ic );
+				sum_base = msc_col + 1;	// any new MSC needs to be to the right of this one's left-end
+			}
+			// or did all of our local sums become invalid?
+//			if( invalid_count == sumI - sum_base )
+//			{
+//				sum_base = sumI + 1;
+//			}
+		}
+		// merge any overlapping MSCs
+		size_t disjoint = cur_msc_list.size() > 0 ? 1 : 0;
+		for( size_t mscI = 1; mscI < cur_msc_list.size(); mscI++ )
+		{
+			if( cur_msc_list[mscI].left_col <= cur_msc_list[mscI-1].right_col )
+			{
+				cur_msc_list[mscI].left_col = cur_msc_list[mscI-1].left_col;
+				cur_msc_list[mscI-1].left_col = (std::numeric_limits<size_t>::max)();
+				cur_msc_list[mscI-1].right_col = (std::numeric_limits<size_t>::max)();
+			}else
+				disjoint++;
+		}
+		std::vector<HssCols> cur_msc_list2( disjoint );
+		size_t mI = 0;
+		for( size_t mscI = 0; mscI < cur_msc_list.size(); mscI++ )
+		{
+			if( cur_msc_list[mscI].left_col != (std::numeric_limits<size_t>::max)() )
+				cur_msc_list2[mI++] = cur_msc_list[mscI];
+		}
+
+		// TODO: grow MSC boundaries to include all surrounding positively scoring regions
+		for( mI = 0; mI < cur_msc_list2.size(); mI++ )
+		{
+			HssCols& hss = cur_msc_list2[mI];
+			// first left, then right
+			size_t lcolI = hss.left_col + 1;
+			for( ; lcolI > 0 && scores[lcolI - 1] > 0; lcolI-- );
+			size_t rcolI = hss.right_col;
+			for( ; rcolI < scores.size() && scores[rcolI] > 0; rcolI++ );
+			hss.left_col = lcolI;
+			hss.right_col = rcolI - 1;
+		}
+		swap( cur_msc_list, cur_msc_list2 );
+
+		// merge overlapping MSCs again
+		// BAD: copied code from above
+
+		disjoint = cur_msc_list.size() > 0 ? 1 : 0;
+		for( size_t mscI = 1; mscI < cur_msc_list.size(); mscI++ )
+		{
+			if( cur_msc_list[mscI].left_col <= cur_msc_list[mscI-1].right_col )
+			{
+				cur_msc_list[mscI].left_col = cur_msc_list[mscI-1].left_col;
+				cur_msc_list[mscI-1].left_col = (std::numeric_limits<size_t>::max)();
+				cur_msc_list[mscI-1].right_col = (std::numeric_limits<size_t>::max)();
+			}else
+				disjoint++;
+		}
+		mI = msc_list.size();
+		msc_list.resize( mI + disjoint );
+		for( size_t mscI = 0; mscI < cur_msc_list.size(); mscI++ )
+		{
+			if( cur_msc_list[mscI].left_col != (std::numeric_limits<size_t>::max)() )
+				msc_list[mI++] = cur_msc_list[mscI];
+		}
+	}
+}
+
+
 inline
 void findHssRandomWalkScoreVector( std::vector< score_t > scores, score_t significance_threshold, hss_list_t& hss_list, uint seqI, uint seqJ, boolean left_homologous, boolean right_homologous )
 {
-	// Invert the scores since we're trying to detect rare bouts of non-homologous sequence
-	for( size_t sI = 0; sI < scores.size(); ++sI )
-		if( scores[sI] != INVALID_SCORE)
-			scores[sI] = -scores[sI];
 
 	score_t left_end_score = left_homologous ? 0 : significance_threshold;
 	score_t right_end_score = right_homologous ? 0 : significance_threshold;
@@ -423,8 +610,7 @@ void findHssRandomWalkScoreVector( std::vector< score_t > scores, score_t signif
 	score_t lrh = score_sum;
 	int64 ladder_point = -1;
 	int64 rev_ladder_point = 0;
-	bool fwd_hss = true;
-	bool homology_exists = true;
+	bool fwd_hss = !left_homologous;
 
 	for( size_t colI = 0; colI <= scores.size(); ++colI )
 	{
@@ -434,6 +620,12 @@ void findHssRandomWalkScoreVector( std::vector< score_t > scores, score_t signif
 		if( colI == scores.size() || (score_sum >= 0 && score_sum + scores[colI] < 0) ||
 			(score_sum >= lrh - significance_threshold && score_sum + scores[colI] < lrh - significance_threshold ) )
 		{
+			if( !fwd_hss && colI == scores.size() && !right_homologous )
+			{
+				fwd_hss = true;
+				if( score_sum <= 0 )
+					ladder_point = colI - 1;
+			}
 			// end of an excursion
 			score_sum = 0;
 			lrh = 0;
@@ -442,7 +634,11 @@ void findHssRandomWalkScoreVector( std::vector< score_t > scores, score_t signif
 				// backtrack to find the MSC in the other direction?
 				// call the entire segment between MSCs the HSS?
 				score_t rev_score_sum = 0;
+				if( colI == scores.size() && !right_homologous )
+					rev_score_sum = significance_threshold;
 				size_t rev_ladder_point = ladder_point;
+				if( colI == scores.size() && !right_homologous )
+					rev_ladder_point = colI - 1;
 				for( size_t rcolI = colI; ((int64)rcolI) > ladder_point && rcolI > 0; --rcolI )
 				{
 					if( scores[rcolI-1] == INVALID_SCORE )
@@ -469,7 +665,6 @@ void findHssRandomWalkScoreVector( std::vector< score_t > scores, score_t signif
 					if( colI == scores.size() && ladder_point == -1 )
 					{
 						rev_ladder_point = scores.size()-1;
-						homology_exists = false;
 					}
 					if( ladder_point == -1 )
 						ladder_point = 0;
@@ -496,8 +691,43 @@ void findHssRandomWalkScoreVector( std::vector< score_t > scores, score_t signif
 		if( score_sum > lrh )
 			lrh = score_sum;
 	}
-//	if( homology_exists )
-//		findRightEndpoint( seqI, seqJ, significance_threshold, scores, hss_list );
+}
+
+template< typename MatchVector >
+void findHssRandomWalk_v2( const MatchVector& iv_list, std::vector< genome::gnSequence* >& seq_table, const PairwiseScoringScheme& scoring, score_t significance_threshold, hss_array_t& hss_array, boolean left_homologous, boolean right_homologous )
+{
+	typedef typename MatchVector::value_type MatchType;
+	if( iv_list.size() == 0 )
+		return;
+	uint seq_count = seq_table.size();
+	hss_array.resize( boost::extents[seq_count][seq_count][iv_list.size()] );
+	for( uint iv_listI = 0; iv_listI < iv_list.size(); iv_listI++ ){
+		const MatchType& iv = iv_list[ iv_listI ];
+		std::vector< std::string > aln_table;
+		GetAlignment( *iv, seq_table, aln_table );
+		
+		for( uint seqI = 0; seqI < seq_count; seqI++ ){
+			uint seqJ;
+			for( seqJ = seqI + 1; seqJ < seq_count; seqJ++ ){
+
+				hss_list_t& hss_list = hss_array[seqI][seqJ][iv_listI];
+				hss_list.clear();
+
+				std::vector< score_t > scores;
+				computeMatchScores( aln_table[seqI], aln_table[seqJ], scoring, scores );
+				computeGapScores( aln_table[seqI], aln_table[seqJ], scoring, scores );
+
+				// Invert the scores since we're trying to detect rare bouts of non-homologous sequence
+				for( size_t sI = 0; sI < scores.size(); ++sI )
+					if( scores[sI] != INVALID_SCORE)
+						scores[sI] = -scores[sI];
+
+				hss_list_t excursion_list;
+				findHssExcursions( scores, significance_threshold, excursion_list, seqI, seqJ, !left_homologous, !right_homologous );
+				findMscFromExcursions( scores, significance_threshold, excursion_list, hss_list, seqI, seqJ, !left_homologous, !right_homologous );
+			}
+		}
+	}
 }
 
 template< typename MatchVector >
@@ -524,6 +754,10 @@ void findHssRandomWalk( const MatchVector& iv_list, std::vector< genome::gnSeque
 				computeMatchScores( aln_table[seqI], aln_table[seqJ], scoring, scores );
 				computeGapScores( aln_table[seqI], aln_table[seqJ], scoring, scores );
 
+				// Invert the scores since we're trying to detect rare bouts of non-homologous sequence
+				for( size_t sI = 0; sI < scores.size(); ++sI )
+					if( scores[sI] != INVALID_SCORE)
+						scores[sI] = -scores[sI];
 				findHssRandomWalkScoreVector( scores, significance_threshold, hss_list, seqI, seqJ, left_homologous, right_homologous );
 			}
 		}
