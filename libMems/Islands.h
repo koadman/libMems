e64231cadc38abf2c2d8584fe0a5ccab03ee3a64
Aaron Darling <aaron.darling@uts.edu.au>
2007-07-29 05:55:30 +0000

    refactoring to move unrelated functions into separate files

--- libMems/Islands.h
+++ libMems/Islands.h
@@ -1,9 +1,9 @@
 /*******************************************************************************
  * $Id: Islands.h,v 1.7 2004/03/01 02:40:08 darling Exp $
- * This file is copyright 2002-2004 Aaron Darling.  All rights reserved.
- * Please see the file called COPYING for licensing, copying, and modification
- * rights.  Redistribution of this file, in whole or in part is prohibited
- * without express permission.
+ * This file is copyright 2002-2007 Aaron Darling and authors listed in the AUTHORS file.
+ * This file is licensed under the GPL.
+ * Please see the file called COPYING for licensing details.
+ * **************
  ******************************************************************************/
 
 #ifndef __Islands_h__
@@ -23,6 +23,7 @@
 #include "libMems/Aligner.h"
 #include <boost/multi_array.hpp>
 #include "libMems/HomologyHMM/homology.h"
+#include "libMems/Scoring.h"
 
 namespace mems {
 
@@ -97,265 +98,8 @@ void simpleFindBackbone( IntervalList& iv_list, uint backbone_size, uint max_gap
  */
 void outputBackbone( const std::vector< GappedAlignment >& backbone_regions, std::ostream& backbone_out );
 
-void allocateDetailList( IntervalList& iv_list, std::vector< std::pair< uint64, uint64 > >& detail_list );
-void allocateDetailList( MatchList& iv_list, std::vector< std::pair< uint64, uint64 > >& detail_list );
-
-void getLCBDetailList( Interval& iv, std::vector< std::pair< uint64, uint64 > >& detail_list );
-
-void getLCBDetailList( MatchList& iv, std::vector< std::pair< uint64, uint64 > >& detail_list );
-
-void PrintDetailList( uint seq_count, const std::vector< std::pair< uint64, uint64 > >& detail_list, std::ostream& os, boolean skip_zeros = true);
-//void PrintDetailList( uint seq_count, vector< pair< uint64, uint64 > >& detail_list, ostream& os, boolean skip_zeros );
-
-
 void getGapBounds( std::vector<gnSeqI>& seq_lengths, std::vector< LCB >& adjacencies, uint seqJ, int leftI, int rightI, int64& left_start, int64& right_start );
 
-void TransformDistanceIdentity( NumericMatrix<double>& identity );
-
-void DistanceMatrix( const MatchList& mlist, NumericMatrix<double>& identity );
-
-
-template< class AbstractMatchVectorType >
-void IdentityMatrix( const AbstractMatchVectorType& matches, const std::vector< genome::gnSequence* >& seq_table, NumericMatrix<double>& identity );
-template<class AbstractMatchType>
-void MatchIdentityMatrix( const AbstractMatchType& amt, const std::vector< genome::gnSequence* >& seq_table, NumericMatrix<double>& identity);
-
-void DistanceMatrix( uint seq_count, const std::vector< std::pair< uint64, uint64 > >& detail_list, NumericMatrix<double>& distance );
-
-void IdentityMatrix( const IntervalList& iv_list, NumericMatrix<double>& identity );
-inline
-void IdentityMatrix( const IntervalList& iv_list, NumericMatrix<double>& identity )
-{
-	std::vector< const AbstractMatch* > am_list;
-	for( size_t ivI = 0; ivI < iv_list.size(); ivI++ )
-		am_list.push_back( &iv_list[ivI] );
-	IdentityMatrix( am_list, iv_list.seq_table, identity );
-}
-
-template< class AbstractMatchVectorType >
-void IdentityMatrix( const AbstractMatchVectorType& matches, const std::vector< genome::gnSequence* >& seq_table, NumericMatrix<double>& identity ){
-	if( matches.size() == 0 )
-		return;
-
-	uint seq_count = seq_table.size();
-	identity = NumericMatrix<double>( seq_count, seq_count );
-	identity.init( 0 );
-	NumericMatrix<double> possible( seq_count, seq_count );
-	possible.init( 0 );
-	
-	for( uint ivI = 0; ivI < matches.size(); ivI++ ){
-		AddToMatchIdentityMatrix( *matches[ ivI ], seq_table, identity );
-	}
-	for( uint seqI = 0; seqI < seq_count; seqI++ ){
-		for( uint seqJ = 0; seqJ < seq_count; seqJ++ ){
-			gnSeqI shorter_len = seq_table[seqI]->length() < seq_table[seqJ]->length() ? seq_table[seqI]->length() : seq_table[seqJ]->length();
-			possible( seqI, seqJ ) += shorter_len;
-		}
-	}
-	identity /= possible;
-}
-
-
-template< class AbstractMatchVectorType >
-void BackboneIdentityMatrix( const AbstractMatchVectorType& matches, const std::vector< genome::gnSequence* >& seq_table, NumericMatrix<double>& identity ){
-	if( matches.size() == 0 )
-		return;
-
-	size_t seq_count = seq_table.size();
-	identity = NumericMatrix<double>( seq_count, seq_count );
-	identity.init( 0 );
-	
-	for( uint ivI = 0; ivI < matches.size(); ivI++ ){
-		AddToMatchIdentityMatrix( *matches[ ivI ], seq_table, identity );
-	}
-
-	NumericMatrix<double> possible( seq_count, seq_count );
-	possible.init( 0 );
-
-	for( size_t mI = 0; mI < matches.size(); ++mI ){
-		std::vector< std::string > alignment;
-		GetAlignment( *(matches[mI]), seq_table, alignment );
-		for( gnSeqI charI = 0; charI < matches[mI]->AlignmentLength(); charI++ ){
-			for( size_t seqI = 0; seqI < seq_count; seqI++ ){
-				for( size_t seqJ = seqI + 1; seqJ < seq_count; seqJ++ ){
-					if( alignment[ seqI ][ charI ] != '-' &&
-						alignment[ seqJ ][ charI ] != '-' ){
-							possible( seqI, seqJ ) += 1;
-					}
-				}
-			}
-		}
-	}
-
-	identity /= possible;
-}
-
-
-template<class AbstractMatchType>
-void MatchIdentityMatrix( const AbstractMatchType& amt, const std::vector< genome::gnSequence* >& seq_table, NumericMatrix<double>& identity)
-{
-	if( amt.SeqCount() == 0 )
-		return;
-	uint seq_count = amt.SeqCount();
-	identity = NumericMatrix<double>( seq_count, seq_count );
-	identity.init( 0 );
-	uint seqI;
-	uint seqJ;
-
-	std::vector< std::string > alignment;
-	GetAlignment( amt, seq_table, alignment );
-	for( gnSeqI charI = 0; charI < amt.AlignmentLength(); charI++ ){
-		for( seqI = 0; seqI < seq_count; seqI++ ){
-			for( seqJ = seqI + 1; seqJ < seq_count; seqJ++ ){
-				if( ( toupper( alignment[ seqI ][ charI ] ) == 
-					toupper( alignment[ seqJ ][ charI ] ) ) &&
-					alignment[ seqI ][ charI ] != '-' ){
-					
-						identity( seqI, seqJ ) += 1;
-				}
-			}
-		}
-	}
-
-	for( seqI = 0; seqI < seq_count; seqI++ ){
-		for( seqJ = seq_count; seqJ > 0; seqJ-- ){
-			if( seqI == seqJ - 1 )
-				// set the diagonal to identical
-				identity( seqI, seqJ - 1 ) = 1;
-			else if( seqI < seqJ - 1 ){
-				// determine the length of the shorter sequence
-				gnSeqI shorter_len = amt.Length( seqI ) < amt.Length( seqJ - 1 ) ? amt.Length( seqI ) : amt.Length( seqJ - 1 );
-				// divide through
-				identity( seqI, seqJ - 1 ) /= (double)shorter_len;
-				// maxes out at 1
-				if( identity( seqI, seqJ - 1 ) > 1 )
-					identity( seqI, seqJ - 1 ) = 1;
-			}else	// copy the other one
-				identity( seqI, seqJ - 1 ) = identity( seqJ - 1, seqI );
-		}
-	}
-}
-
-
-
-template<class AbstractMatchType>
-void AddToMatchIdentityMatrix( const AbstractMatchType& amt, const std::vector< genome::gnSequence* >& seq_table, NumericMatrix<double>& identity)
-{
-	if( amt.SeqCount() == 0 )
-		return;
-	uint seq_count = amt.SeqCount();
-	uint seqI;
-	uint seqJ;
-
-	std::vector< std::string > alignment;
-	GetAlignment( amt, seq_table, alignment );
-	for( gnSeqI charI = 0; charI < amt.AlignmentLength(); charI++ ){
-		for( seqI = 0; seqI < seq_count; seqI++ ){
-			for( seqJ = seqI + 1; seqJ < seq_count; seqJ++ ){
-				if( ( toupper( alignment[ seqI ][ charI ] ) == 
-					toupper( alignment[ seqJ ][ charI ] ) ) &&
-					alignment[ seqI ][ charI ] != '-' ){
-					
-						identity( seqI, seqJ ) += 1;
-				}
-			}
-		}
-	}
-}
-
-/*
-// template specialization for (exact) matches
-inline
-void AddToMatchIdentityMatrix( const Match& m, const std::vector< genome::gnSequence* >& seq_table, NumericMatrix<double>& identity)
-{
-	if( m.SeqCount() == 0 )
-		return;
-	for( uint seqI = 0; seqI < m.SeqCount(); seqI++ )
-		if( m.LeftEnd(seqI) != NO_MATCH )
-			for( uint seqJ = seqI + 1; seqJ < m.SeqCount(); seqJ++ )
-				if( m.LeftEnd(seqJ) != NO_MATCH )
-					identity(seqI,seqJ) += m.Length();
-}
-*/
-
-template< typename MatchVector >
-void SingleCopyDistanceMatrix( MatchVector& iv_list, std::vector< genome::gnSequence* >& seq_table, NumericMatrix<double>& distance )
-{
-	uint seq_count = seq_table.size();
-	distance = NumericMatrix<double>( seq_count, seq_count );
-	distance.init( 0 );
-	uint seqI;
-	uint seqJ;
-	std::vector< std::pair< bitset_t, bitset_t > > tmp_comp( seq_count );
-	std::vector< std::vector< std::pair< bitset_t, bitset_t > > > pair_comp( seq_count, tmp_comp );
-	for( uint seqI = 0; seqI < seq_count; ++seqI )
-	{
-		for( uint seqJ = seqI+1; seqJ < seq_count; ++seqJ )
-		{
-			pair_comp[seqI][seqJ].first.resize( seq_table[seqI]->length(), false );
-			pair_comp[seqI][seqJ].second.resize( seq_table[seqJ]->length(), false );
-		}
-	}
-	for( size_t ivI = 0; ivI < iv_list.size(); ++ivI )
-	{
-		std::vector< bitset_t > aln_table;
-		iv_list[ivI]->GetAlignment(aln_table);
-		for( uint seqI = 0; seqI < seq_count; ++seqI )
-		{
-			for( uint seqJ = seqI+1; seqJ < seq_count; ++seqJ )
-			{
-				gnSeqI seqI_pos = iv_list[ivI]->LeftEnd(seqI);
-				gnSeqI seqJ_pos = iv_list[ivI]->LeftEnd(seqJ);
-				AbstractMatch::orientation o_i = iv_list[ivI]->Orientation(seqI);
-				AbstractMatch::orientation o_j = iv_list[ivI]->Orientation(seqJ);
-				if( o_i == AbstractMatch::reverse )
-					seqI_pos = iv_list[ivI]->RightEnd(seqI);
-				if( o_j == AbstractMatch::reverse )
-					seqJ_pos = iv_list[ivI]->RightEnd(seqJ);
-				if( seqI_pos == NO_MATCH || seqJ_pos == NO_MATCH )
-					continue;
-				for( size_t colI = 0; colI < aln_table[seqI].size(); ++colI )
-				{
-					if( aln_table[seqI].test(colI) && aln_table[seqJ].test(colI) )
-					{
-						pair_comp[seqI][seqJ].first.set(seqI_pos-1,true);
-						pair_comp[seqI][seqJ].second.set(seqJ_pos-1,true);
-					}
-					if( aln_table[seqI].test(colI) )
-						if( o_i == AbstractMatch::forward )
-							seqI_pos++;
-						else
-							seqI_pos--;
-					if( aln_table[seqJ].test(colI) )
-						if( o_j == AbstractMatch::forward )
-							seqJ_pos++;
-						else
-							seqJ_pos--;
-				}
-			}
-		}
-	}
-	for( uint seqI = 0; seqI < seq_count; ++seqI )
-	{
-		for( uint seqJ = seqI+1; seqJ < seq_count; ++seqJ )
-		{
-			double pI = ((double)pair_comp[seqI][seqJ].first.count())/((double)pair_comp[seqI][seqJ].first.size());
-			double pJ = ((double)pair_comp[seqI][seqJ].second.count())/((double)pair_comp[seqI][seqJ].second.size());
-			distance(seqI,seqJ) = (pI + pJ) / 2.0;
-			distance(seqJ,seqI) = (pI + pJ) / 2.0;
-		}
-	}
-	TransformDistanceIdentity(distance);
-}
-
-static const score_t INVALID_SCORE = (std::numeric_limits<score_t>::max)();
-
-//tjtaed: function to compute the SP column score, and cumulative SP score from an alignment
-void computeSPScore( const std::vector<std::string>& alignment, const PairwiseScoringScheme& pss, std::vector<score_t>& scores, score_t& score );
-//tjt: function to compute the consensus column score, consensus sequence, and cumulative consensus score from an alignment
-void computeConsensusScore( const std::vector<std::string>& alignment, const PairwiseScoringScheme& pss, std::vector<score_t>& scores, std::string& consensus, score_t& score );
-void computeMatchScores( const std::string& seq1, const std::string& seq2, const PairwiseScoringScheme& scoring, std::vector<score_t>& scores );
-void computeGapScores( const std::string& seq1, const std::string& seq2, const PairwiseScoringScheme& scoring, std::vector<score_t>& scores );
 
 inline
 void findRightEndpoint( size_t seqI, size_t seqJ, score_t significance_threshold, std::vector< score_t >& scores, hss_list_t& hss_list )
@@ -618,16 +362,27 @@ char* getCharmap()
 	charmap['G'] = 2;
 	charmap['T'] = 3;
 	charmap['-'] = 4;
+	initialized = true;
 	return charmap;
 }
-static char colmap[5][5];
+// a mapping from pairwise alignment columns to HomologyHMM emission codes
+// row/column indices are as given by the charmap above (ACGT- == 01234).
+static char colmap[5][5] = {
+//    A   C   G   T   -
+	{'1','3','4','5','7'},	// A
+	{'3','2','6','4','7'},  // C
+	{'4','6','2','3','7'},  // G
+	{'5','4','3','1','7'},  // T
+	{'7','7','7','7','7'},  // -
+};
+
 inline
 void getColmap()
 {
+/*
 	static bool initialized = false;
 	if(initialized)
 		return;
-	initialized = true;
 
 	colmap[0][0] = '1';
 	colmap[3][3] = '1';
@@ -651,6 +406,8 @@ void getColmap()
 		colmap[4][i] = '7';
 	}
 	colmap[4][4] = 0;	// undefined state
+	initialized = true;
+*/
 }
 
 inline
